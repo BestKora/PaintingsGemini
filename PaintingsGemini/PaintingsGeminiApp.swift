79a33ca3bcb4272861a8841979f8833f41d1c6be
BestKora <developer@bestkora.com>
2026-02-07 14:02:03 +0300

    Add most paintings view toggle

--- PaintingsGemini/PaintingsGeminiApp.swift
+++ PaintingsGemini/PaintingsGeminiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PaintingsGeminiApp: App {
     var body: some Scene {
         WindowGroup {
-            PaintingsGeminiView()
+            MostPaintingsView()
         }
     }
 }
